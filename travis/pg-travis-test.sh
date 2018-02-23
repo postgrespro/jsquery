60c696e75dd22688a5cac903034117c7be5338f5
Alexander Korotkov <a.korotkov@postgrespro.ru>
2018-02-23 12:50:52 +0300

    Run travis-ci with valgrind.

--- travis/pg-travis-test.sh
+++ travis/pg-travis-test.sh
@@ -4,19 +4,6 @@ set -eux
 
 sudo apt-get update
 
-
-# required packages
-apt_packages="postgresql-$PG_VER postgresql-server-dev-$PG_VER postgresql-common build-essential flex bison"
-
-# exit code
-status=0
-
-# pg_config path
-pg_ctl_path=/usr/lib/postgresql/$PG_VER/bin/pg_ctl
-initdb_path=/usr/lib/postgresql/$PG_VER/bin/initdb
-config_path=/usr/lib/postgresql/$PG_VER/bin/pg_config
-
-
 # bug: http://www.postgresql.org/message-id/20130508192711.GA9243@msgid.df7cb.de
 sudo update-alternatives --remove-all postmaster.1.gz
 
@@ -26,12 +13,41 @@ sudo service postgresql stop
 echo 'exit 0' | sudo tee /etc/init.d/postgresql
 sudo chmod a+x /etc/init.d/postgresql
 
-# install required packages
-sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y install -qq $apt_packages
+# install PostgreSQL
+if [ $CHECK_TYPE = "valgrind" ]; then
+	# install required packages
+	apt_packages="build-essential libgd-dev valgrind lcov"
+	sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y install -qq $apt_packages
+	# grab sources from github
+	tag=`curl -s 'https://api.github.com/repos/postgres/postgres/git/refs/tags' | jq -r '.[] | .ref' | sed 's/^refs\/tags\///' | grep "REL_*${PG_VER/./_}_" | tail -n 1`
+	prefix="$HOME/pgsql-$tag"
+	curl "https://codeload.github.com/postgres/postgres/tar.gz/$tag" -o ~/$tag.tar.gz
+	# build them with valgrind support
+	pushd ~
+	tar -xzf "$tag.tar.gz"
+	cd "postgres-$tag"
+	./configure --enable-debug --enable-cassert --enable-coverage --prefix=$prefix
+	sed -i.bak "s/\/* #define USE_VALGRIND *\//#define USE_VALGRIND/g" src/include/pg_config_manual.h
+	make -sj4
+	make -sj4 install
+	popd
+	export PATH="$prefix/bin:$PATH"
+else
+	apt_packages="postgresql-$PG_VER postgresql-server-dev-$PG_VER postgresql-common build-essential libgd-dev"
+	sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y install -qq $apt_packages
+	prefix=/usr/lib/postgresql/$PG_VER
+fi
 
+# config path
+pg_ctl_path=$prefix/bin/pg_ctl
+initdb_path=$prefix/bin/initdb
+config_path=$prefix/bin/pg_config
+
+# exit code
+status=0
 
 # perform code analysis if necessary
-if [ $CHECK_CODE = "true" ]; then
+if [ $CHECK_TYPE = "static" ]; then
 
 	if [ "$CC" = "clang" ]; then
 		sudo apt-get -y install -qq clang-$LLVM_VER
@@ -72,9 +88,14 @@ $initdb_path -D $CLUSTER_PATH -U $USER -A trust
 ulimit -c unlimited -S
 echo '/tmp/%e-%s-%p.core' | sudo tee /proc/sys/kernel/core_pattern
 
-# build jsquery (using CFLAGS_SL for gcov)
-make USE_PGXS=1 PG_CONFIG=$config_path CFLAGS_SL="$($config_path --cflags_sl) -coverage"
-sudo make install USE_PGXS=1 PG_CONFIG=$config_path
+# build extension (using CFLAGS_SL for gcov)
+if [ $CHECK_TYPE == "valgrind" ] && [ $CC = "clang" ]; then
+	make USE_PGXS=1 USE_ASSERT_CHECKING=1 PG_CONFIG=$config_path
+	make install USE_PGXS=1 PG_CONFIG=$config_path
+else
+	make USE_PGXS=1 USE_ASSERT_CHECKING=1 PG_CONFIG=$config_path CFLAGS_SL="$($config_path --cflags_sl) -coverage"
+	sudo make install USE_PGXS=1 PG_CONFIG=$config_path
+fi
 
 # check build
 status=$?
@@ -85,13 +106,32 @@ sudo chown $USER /var/run/postgresql/
 
 # start cluster 'test'
 echo "port = 55435" >> $CLUSTER_PATH/postgresql.conf
-$pg_ctl_path -D $CLUSTER_PATH start -l postgres.log -w
+if [ $CHECK_TYPE = "valgrind" ]; then
+	PGCTLTIMEOUT=600 \
+	valgrind --leak-check=no --gen-suppressions=all \
+	--suppressions=$HOME/postgres-$tag/src/tools/valgrind.supp --time-stamp=yes \
+	--log-file=/tmp/pid-%p.log --trace-children=yes \
+	$pg_ctl_path -D $CLUSTER_PATH start -l postgres.log -w
+else
+	$pg_ctl_path -D $CLUSTER_PATH start -l postgres.log -w
+fi
 
 # run regression tests
 PGPORT=55435 PGUSER=$USER PG_CONFIG=$config_path make installcheck USE_PGXS=1 || status=$?
 
 # show diff if it exists
 if test -f regression.diffs; then cat regression.diffs; fi
+
+# show valgrind logs if needed
+if [ $CHECK_TYPE = "valgrind" ]; then
+	for f in ` find /tmp -name pid-*.log ` ; do
+		if grep -q 'Command: [^ ]*/postgres' $f && grep -q 'ERROR SUMMARY: [1-9]' $f; then
+			echo "========= Contents of $f"
+			cat $f
+			status=1
+		fi
+	done
+fi
 
 # check core dumps if any
 for corefile in $(find /tmp/ -name '*.core' 2>/dev/null) ; do
@@ -100,7 +140,14 @@ for corefile in $(find /tmp/ -name '*.core' 2>/dev/null) ; do
 	gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" $binary $corefile
 done
 
+# stop cluster
+$pg_ctl_path -D $CLUSTER_PATH stop -l postgres.log -w
+
 #generate *.gcov files
-gcov *.c *.h
+if [ $CHECK_TYPE == "valgrind" ] && [ $CC = "clang" ]; then
+	bash <(curl -s https://codecov.io/bash) -x "llvm-cov gcov"
+else
+	bash <(curl -s https://codecov.io/bash)
+fi
 
 exit $status
