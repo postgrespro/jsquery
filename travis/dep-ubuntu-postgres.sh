b57b1acac09c927d75a92352831900946cea2ee4
Ekaterina Sokolova <e.sokolova@postgrespro.ru>
2024-07-05 12:48:42 +0300

    Attempt to revive Travis.

--- travis/dep-ubuntu-postgres.sh
+++ travis/dep-ubuntu-postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cat ./travis/postgresql.gpg.key | sudo apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main $PG_VER" | sudo tee /etc/apt/sources.list.d/pgdg.list
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo "deb http://apt-archive.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main $PG_VER"
