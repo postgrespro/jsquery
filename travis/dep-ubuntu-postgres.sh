c6a07adb886ba01cc6875c7ba26cb58398eef664
Ekaterina Sokolova <e.sokolova@postgrespro.ru>
2024-10-23 15:45:42 +0300

    Second attempt to revive Travis + update copyright.

--- travis/dep-ubuntu-postgres.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
-echo "deb http://apt-archive.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main $PG_VER"
